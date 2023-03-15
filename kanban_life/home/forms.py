dbb10a94874a893f6e3edfab6d079d7194888cdf
Pichau <gustavosandrindattein123@gmail.com>
2023-03-15 20:39:02 -0300

    configurado os arquivos no AWS e o banco de dados postgres

---
+++ kanban_life/home/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class UsuarioForm(forms.ModelForm):
+
+    password = forms.CharField(
+        label='Senha',
+        required=True,
+    )
+
+    class Meta:
+        model = User
+
+        exclude = [
+            'id', 'last_login', 'date_joined'
+        ]
--- kanban_life/home/views.py
+++ kanban_life/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from kanban_life.home.models import User
 
 
 def home(request):
@@ -13,4 +14,14 @@ def about(request):
 
 
 def sign_up(request):
+    if request.POST:
+        nome_completo = request.POST['nome_completo']
+        email = request.POST['email']
+        senha = request.POST['senha']
+        user = User()
+        user.password = senha
+        user.email = email
+        user.first_name = nome_completo
+        user.save()
+
     return render(request, 'home/sign_up.html')
