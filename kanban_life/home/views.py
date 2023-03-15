06622b6c1ef45fa648d4e76d2bc89c9c3dedca3c
Pichau <gustavosandrindattein123@gmail.com>
2023-03-15 08:17:48 -0300

    Criado o formulário da criação de conta, proximo passo criar a conta com as informações que vao vir

--- kanban_life/home/views.py
+++ kanban_life/home/views.py
@@ -10,3 +10,7 @@ def home(request):
 
 def about(request):
     return render(request, 'home/about.html')
+
+
+def sign_up(request):
+    return render(request, 'home/sign_up.html')
--- kanban_life/urls.py
+++ kanban_life/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from kanban_life.home.views import home, about
+from kanban_life.home.views import home, about, sign_up
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('about/', about, name='about'),
+    path('sign-up/', sign_up, name='sign_up'),
 ]
