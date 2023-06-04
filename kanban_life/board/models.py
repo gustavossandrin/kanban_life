717553f83609c6ed3a9cbbc2e87757fa427990a9
Pichau <gustavosandrindattein123@gmail.com>
2023-06-04 14:19:19 -0300

    Criado um model para as colunas onde vão ficar as tarefas

--- kanban_life/board/models.py
+++ kanban_life/board/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class TarefaColuna(models.Model):
+    nome = models.CharField(
+        u'Nome',
+        max_length=30
+    )
+    user = models.ForeignKey(
+        'home.User',
+        on_delete=models.CASCADE
+    )
+    posicao = models.IntegerField(
+        u'Posição Coluna',
+        blank=False,
+        null=False,
+    )
+    maximo_de_tarefas = models.IntegerField(
+        u'Número Máximo de Tarefas',
+        blank=True,
+        null=True,
+    )
+    coluna_final = models.BooleanField(
+        u'Coluna Final',
+        default=False,
+    )
+    #  Dados de LOG
+    criado_em = models.DateTimeField(
+        u'Criado em',
+        blank=True,
+        null=True,
+        auto_now_add=True,
+    )
+    alterado_em = models.DateTimeField(
+        u'Alterado em',
+        blank=True,
+        null=True,
+        auto_now=True,
+    )
+
+
